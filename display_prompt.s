5156a7a42581fb066b4074ebd8a69a270f39d619
Timothy Kirathe <kirathetimothy1@gmail.com>
2024-12-30 07:14:51 +0000

    Rename display-prompt.s to display_prompt.s

--- display_prompt.s
+++ display_prompt.s
@@ -11,6 +11,13 @@ display_prompt:
     sub sp, #8000
     mov r0, #1
     ldr r1, =prompt
+
+_size_of_string:
+    mov r2, #0
+    ldrb r3, [r1, r2]
+    cmp r3, #0
+    beq _print_prompt
+
     mov r2, #8
     mov r7, #0x04
     svc #0
